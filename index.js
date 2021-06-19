9d9c62383e96098d7c04cf7daf57726e7f7f4613
Fengwei Wang <foomango@gmail.com>
2021-06-19 01:10:40 -0400

    update documentation

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 // @name         Bypass taobao overseas block(绕过淘宝屏蔽海外用户)
 // @description  当访问禁止出口的淘宝商品时，取消自动跳转
 // @icon         https://img.alicdn.com/favicon.ico
-// @version      0.2.0
+// @version      0.2.1
 // @author       foomango
 // @match        *item.taobao.com/*
 // @match        *detail.tmall.com/*
 // @grant        none
 // @namespace    https://greasyfork.org/users/705411-foomango
-// @run-at       document-body
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {
@@ -23,7 +23,7 @@
         return event.returnValue = 'Are you sure you want to exit?'
         /*eslint-enable */
     }
-    alert('请快速点击确定（OK）按钮3次')
+    alert('点击确定（OK）按钮后，请在屏幕上快速移动并随机连续点击几次。')
     addEventListener('beforeunload', beforeUnloadListener, {capture: true});
 
     console.log('Finish loading bypass-taobao-overseas-block.js')
